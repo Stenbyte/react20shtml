9ff6fd6b533821c5e45ecffb45445993852a5396
Sten Scheifel <sten.scheifel@gmail.com>
2020-10-09 14:54:57 +0300

    final

---
+++ Portfolio_website/main.js
@@ -0,0 +1,23 @@
+const backToButton = document.getElementById
+("back-to-top");
+const width = window.matchMedia("(max-width: 576px)");
+
+
+
+const backToTop = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
+
+window.onscroll = () => scrollCheck();
+
+const scrollCheck = () => {
+if (
+    document.body.scrollTop > 200 ||
+    (document.documentElement.scrollTop > 200 && !width.matches)
+) {
+    backToButton.style.display = "flex";
+} else {
+    backToButton.style.display = "none";
+  }
+};
